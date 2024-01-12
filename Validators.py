10cf3fdf153c682884ff8180b81cf729c4acc77c
joaosantosmedeiros <jopesame@gmail.com>
2024-01-12 11:57:17 -0300

    feat: add data, hour and user validation

--- Validators.py
+++ Validators.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date 
+
 def validarNome():
     while True:
         nome = input('Digite o nome do paciente: ')
@@ -22,3 +24,61 @@ def validarTelefone():
                     ehValido = False
             if (ehValido):
                 return telefone
+
+
+def validarHora(hora):
+    if len(hora) > 2 or len(hora) < 1:
+        return False
+    for caracter in hora:
+        if caracter.isdigit() == False:
+            return False
+    hora = int(hora)
+    if hora < 0 or hora > 23:
+        return False
+    return True
+
+
+def validarData(data):
+    if data.count("/") < 2:
+        return False
+    [dia, mes, ano] = (data.split("/"))
+
+    for caracter in dia:
+        if caracter.isdigit() == False:
+            return False
+    dia = int(dia)
+
+    for caracter in mes:
+        if caracter.isdigit() == False:
+            return False
+    mes = int(mes)
+
+    for caracter in ano:
+        if caracter.isdigit() == False:
+            return False
+    ano = int(ano)
+
+    try:
+        datetime(ano, mes, dia)
+    except ValueError:
+        return False
+
+    if ano < datetime.now().year:
+        return False
+    if ano == datetime.now().year:
+        if mes < datetime.now().month:
+            return False
+        if mes == datetime.now().month:
+            if dia < datetime.now().day:
+                return False
+    
+    return True
+
+
+def validarUsuario(usuario):
+    for caracter in usuario:
+            if caracter.isdigit() == False:
+                print('Erro! Digite um número válido!')
+                return False
+    return True
+    
